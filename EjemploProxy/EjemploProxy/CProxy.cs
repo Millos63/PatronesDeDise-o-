5cc5a9e912b17bec8dc4fedea86a28900e027b1a
Emigdio Espinosa <millosespinosa@gmail.com>
2022-09-05 13:39:13 -0500

    Se termino de crear el ejemplo de proxy, el ejemplo se hizo con un invernadero

---
+++ EjemploProxy/EjemploProxy/CProxy.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjemploProxy
+{
+    public class CProxy
+    {
+        public interface ISujeto
+        {
+            void Peticion(int pOpcion);
+        }
+
+        public class ProxySencillo : ISujeto
+        {
+            private CInvernadero invernadero;
+
+            public void Peticion(int pOpcion)
+            {
+                if (invernadero == null)
+                {
+                    Console.WriteLine("Activando el sujeto");
+                    invernadero = new CInvernadero();
+                }
+                if (pOpcion == 1)
+                    invernadero.Carga();
+                if (pOpcion == 2)
+                    invernadero.CamionesLlenos(3);
+            }
+        }
+
+        public class ProxySeguro : ISujeto
+        {
+            private CInvernadero invernadero;
+
+            public void Peticion(int pOpcion)
+            {
+                string password;
+
+                Console.WriteLine("Dame el password");
+                password = Console.ReadLine();
+
+                if(password == "rojos123")
+                {
+                    if(invernadero == null)
+                    {
+                        Console.WriteLine("Activando el sujeto");
+                        invernadero = new CInvernadero();
+                    }
+
+                    if (pOpcion == 1)
+                        invernadero.Carga();
+                    if (pOpcion == 2)
+                        invernadero.CamionesLlenos(3);
+                }
+                else
+                {
+                    Console.WriteLine("Acceso denegado");
+                }
+            }
+        }
+
+        //Esta es la clase protegida
+        private class CInvernadero
+        {
+            public void Carga()
+            {
+                Console.WriteLine("100 XL");
+                Console.WriteLine("200 L");
+                Console.WriteLine("50 M");
+                Console.WriteLine("50 S");
+            }
+
+            public void CamionesLlenos(int n)
+            {
+                Console.WriteLine("Cargando {0} camiones", n);
+            }
+        }
+
+
+    }
+}
--- EjemploProxy/EjemploProxy/Program.cs
+++ EjemploProxy/EjemploProxy/Program.cs
@@ -10,6 +10,19 @@ namespace EjemploProxy
     {
         static void Main(string[] args)
         {
+            CProxy.ISujeto miProxy = new CProxy.ProxySencillo();
+
+            miProxy.Peticion(1);
+            Console.WriteLine("------");
+            miProxy.Peticion(2);
+
+            Console.WriteLine("-------");
+
+            CProxy.ISujeto miProxyS = new CProxy.ProxySeguro();
+
+            miProxy.Peticion(1);
+            Console.WriteLine("------");
+            miProxyS.Peticion(2);
         }
     }
 }

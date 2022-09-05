68113bfff1c46250eb77c08dc32f197c7c30ec1d
Emigdio Espinosa <millosespinosa@gmail.com>
2022-09-05 13:00:18 -0500

    Terminamos el proyecto Proxy02 Y vamos a empezar a crear el nuestro como ejemplo

--- Proxy02/Proxy02/CProxy.cs
+++ Proxy02/Proxy02/CProxy.cs
@@ -6,7 +6,82 @@ using System.Threading.Tasks;
 
 namespace Proxy02
 {
-    class CProxy
+    //Haremos uso de anidamiento adentro de la clase
+    //Esto hace que cualquier clase colocada como privada ya anidada
+    //solo sea conocida por CProxy
+    public class CProxy
     {
+        public interface ISujeto
+        {
+            void Peticion(int pOpcion);
+        }
+
+        public class ProxySencillo:ISujeto
+        {
+            private CCocina cocina;
+
+            public void Peticion(int pOpcion)
+            {
+                if(cocina==null)
+                {
+                    Console.WriteLine("Activando el sujeto");
+                    cocina = new CCocina();
+                }
+                if (pOpcion == 1)
+                    cocina.RecetaSecreta();
+                if (pOpcion == 2)
+                    cocina.Cocinar(5);
+            }
+        }
+
+        public class ProxySeguro : ISujeto
+        {
+            private CCocina cocina;
+
+            public void Peticion(int pOpcion)
+            {
+                string password;
+
+                Console.WriteLine("Dame el password");
+                password = Console.ReadLine();
+
+                if (password == "abc123")
+                {
+                    if (cocina == null)
+                    {
+                        Console.WriteLine("Activando el sujeto");
+                        cocina = new CCocina();
+                    }
+
+
+                    if (pOpcion == 1)
+                        cocina.RecetaSecreta();
+                    if (pOpcion == 2)
+                        cocina.Cocinar(5);
+                }
+                else
+                {
+                    Console.WriteLine("Acceso denegado");
+                }
+            }  
+        }
+
+        //Esta es la clase 
+        private class CCocina
+        {
+            public void RecetaSecreta()
+            {
+                Console.WriteLine("Pan");
+                Console.WriteLine("Aceite de oliva");
+                Console.WriteLine("Especias");
+                Console.WriteLine("Jamon");
+                Console.WriteLine("Queso");
+            }
+
+            public void Cocinar(int n)
+            {
+                Console.WriteLine("Cocinando {0} recetas", n);
+            }
+        }
     }
 }
--- Proxy02/Proxy02/Program.cs
+++ Proxy02/Proxy02/Program.cs
@@ -10,6 +10,23 @@ namespace Proxy02
     {
         static void Main(string[] args)
         {
+            CProxy.ISujeto miProxy = new CProxy.ProxySencillo();
+
+            //No podemos acceder a la clase
+            //CProxy.CCocina
+
+            miProxy.Peticion(1);
+            Console.WriteLine("------");
+            miProxy.Peticion(2);
+
+            Console.WriteLine("-------");
+
+            CProxy.ISujeto miProxyS = new CProxy.ProxySeguro();
+
+            miProxy.Peticion(1);
+            Console.WriteLine("------");
+            miProxyS.Peticion(2);
+
         }
     }
 }

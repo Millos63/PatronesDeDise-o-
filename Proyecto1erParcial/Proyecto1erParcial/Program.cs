e0e183d70f9ba35b9ce71f55062bac2dcda8f1a6
Emigdio Espinosa <millosespinosa@gmail.com>
2022-09-07 12:53:32 -0500

    Creamos la interfaz de usuario, ahora crearemos una nueva rama para trabajar con el patron Estrategia

--- Proyecto1erParcial/Proyecto1erParcial/Program.cs
+++ Proyecto1erParcial/Proyecto1erParcial/Program.cs
@@ -8,8 +8,34 @@ namespace Proyecto1erParcial
 {
     class Program
     {
+
         static void Main(string[] args)
         {
+            //variables
+            string opc = "";
+
+            //Interfaz de usuario
+            while(opc != "4")
+            {
+                Console.WriteLine("---------ROJOS-----------\n\r" +
+                "Bienvenido a la interfaz de usuario para la empresa ROJOS.\n\r" +
+                "Cuál es la acción que le gustaria realizar?\n\r" +
+                "1. Seleccionar carga. \n\r" +
+                "2. Construir un nuevo invernadero \n\r" +
+                "3. Pagar nomina \n\r" +
+                "4. Salir");
+                opc = Console.ReadLine();
+
+                //1. Seleccionar carga
+                if(opc == "1")
+                {
+
+                }
+
+
+            }
+
+
         }
     }
 }

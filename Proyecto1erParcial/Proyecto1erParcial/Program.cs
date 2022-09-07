875c564bde10d647079d13c9b64bf8ee5de07082
Emigdio Espinosa <millosespinosa@gmail.com>
2022-09-07 13:33:49 -0500

    Creamos la interfaz IOperacion, y creamos la interfaz para la selecionadora

--- Proyecto1erParcial/Proyecto1erParcial/Program.cs
+++ Proyecto1erParcial/Proyecto1erParcial/Program.cs
@@ -29,7 +29,66 @@ namespace Proyecto1erParcial
                 //1. Seleccionar carga
                 if(opc == "1")
                 {
+                    //Variables
+                    int xl = 0;
+                    int l = 0;
+                    int m = 0;
+                    int s = 0;
+                    int c = 0;
+                    int color = 0;
+                    string leer = "";
+                    
 
+                    Console.WriteLine("\n\r Bienvenido a la seleccionadora de ROJOS" +
+                        "\n\r Vamos a empezar a seleccionar el jitomate. Cuántos jitomates tienes de:" +
+                        "\n\rXL:");
+                    leer = Console.ReadLine();
+                    xl = Convert.ToInt32(leer);
+
+                    Console.WriteLine("\n\rL:");
+                    leer = Console.ReadLine();
+                    l = Convert.ToInt32(leer);
+
+                    Console.WriteLine("\n\rM:");
+                    leer = Console.ReadLine();
+                    m = Convert.ToInt32(leer);
+
+                    Console.WriteLine("\n\rS:");
+                    leer = Console.ReadLine();
+                    s = Convert.ToInt32(leer);
+
+                    Console.WriteLine("\n\rC:");
+                    leer = Console.ReadLine();
+                    c = Convert.ToInt32(leer);
+
+                    Console.WriteLine("De qué color son los jitomates seleccionados?\n\r" +
+                        "1. Verdes, 2. Inicio de color, 3. Naranjas, 4. Rojos ");
+                    leer = Console.ReadLine();
+                    color = Convert.ToInt32(leer);
+
+                    //Verdes
+                    if(color == 1)
+                    {
+                        
+                    }
+
+                    //Inicio de color
+                    if(color == 2)
+                    {
+
+                    }
+
+                    //Naranjas
+                    if(color == 3)
+                    {
+
+                    }
+
+                    //Rojos
+                    if(color == 4)
+                    {
+
+                    }
                 }
 
 

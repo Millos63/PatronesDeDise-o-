2d046bef6d8b736c2884abbf182130b3f12de4b9
Emigdio Espinosa <millosespinosa@gmail.com>
2022-09-21 13:42:29 -0500

    Terminamos el programa de Bridge03

--- Bridge03/Bridge03/Program.cs
+++ Bridge03/Bridge03/Program.cs
@@ -10,6 +10,30 @@ namespace Bridge03
     {
         static void Main(string[] args)
         {
+            //Main representa al cliente
+            Dictionary<string, double> productos = new Dictionary<string, double>();
+
+            //Estos son los productos a usar
+            productos.Add("M101", 56.32);
+            productos.Add("M234", 23.88);
+            productos.Add("C654", 974.56);
+            productos.Add("M401", 43.28);
+            productos.Add("C345", 785.12);
+            productos.Add("D567", 432.56);
+            productos.Add("M103", 874.54);
+            productos.Add("D901", 23.18);
+            productos.Add("C732", 43.12);
+            productos.Add("M056", 21.42);
+
+            //Creamos el bridge
+            //CAbstraccion bridge = new CAbstraccion(new CImplementacion1(), productos);
+
+            CAbstraccion bridge = new CAbstraccion(3, productos);
+
+            bridge.MostrarTotales();
+            bridge.Listar();
+
+
         }
     }
 }

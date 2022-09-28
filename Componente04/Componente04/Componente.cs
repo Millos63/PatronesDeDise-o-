446409448cf093758c165aae813487a7c80017e7
Emigdio Espinosa <millosespinosa@gmail.com>
2022-09-28 14:05:20 -0500

    Terminamos el Bridge03

---
+++ Componente04/Componente04/Componente.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Componente04
+{
+    class CComponente<T> : IComponente<T>
+    {
+        //Identificador del componente
+        public T Nombre { get; set; }
+
+        public CComponente(T pNombre)
+        {
+            Nombre = pNombre;
+        }
+
+        //Este lo adicionamos para que no nos marque error la interfaz, pero realmente no se ocupa aquí
+        public void Adicionar(IComponente<T> pElemento)
+        {
+            Console.WriteLine("Solo se adiciona a los compuestos, no a los componentes");
+        }
+
+        public IComponente<T> Borrar(T pElemento)
+        {
+            Console.WriteLine("No se puede eliminar directamente");
+            return this;
+        }
+
+        public IComponente<T> Buscar(T pElemento)
+        {
+            if (pElemento.Equals(Nombre))
+                return this;
+            else
+                return null;
+        }
+
+        public string Mostrar(int pProfundidad)
+        {
+            //Colocamos la cantidad de - según la profundidad
+            return new String('-', pProfundidad) + Nombre + "\n\r";
+
+        }
+    }
+}
---
+++ Componente04/Componente04/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Componente04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IComponente<string> arbol = new CCompuesto<string>("root");
+            IComponente<string> trabajo = arbol;
+
+            string opcion = "";
+            string dato = "";
+
+            while (opcion != "6")
+            {
+                Console.WriteLine("Estoy en {0}", trabajo.Nombre);
+                Console.WriteLine("1. Adicionar compuesto, 2. Adicionar componente 3. Borrar, 4.Buscar, 5.Mostrar, 6.Salir");
+                opcion = Console.ReadLine();
+                Console.WriteLine("----------------");
+
+                if(opcion == "1")
+                {
+                    Console.WriteLine("Dame el nombre del compuesto: ");
+                    dato = Console.ReadLine();
+
+                    IComponente<string> c = new CCompuesto<string>(dato);
+                    trabajo.Adicionar(c);
+                    trabajo = c;
+                }
+                if (opcion == "2")
+                {
+                    Console.WriteLine("Dame el valor del compnente: ");
+                    dato = Console.ReadLine();
+
+                    trabajo.Adicionar(new CCompuesto<string>(dato));
+                }
+
+                if (opcion == "3")
+                {
+                    Console.WriteLine("Dame el elemento a borrar: ");
+                    dato = Console.ReadLine();
+
+                    trabajo = trabajo.Borrar(dato);
+                }
+
+                if(opcion == "4")
+                {
+                    Console.WriteLine("Dame el elemento a encontrar: ");
+                    dato = Console.ReadLine();
+                    trabajo = arbol.Buscar(dato);
+                }
+                if(opcion=="5")
+                {
+                    Console.WriteLine(arbol.Mostrar(0));
+                }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            }
+
+
+        }
+    }
+}

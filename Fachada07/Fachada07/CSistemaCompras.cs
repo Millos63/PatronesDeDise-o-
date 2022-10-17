64533bd1f116c19bae5021edb2f67d9a5f9f55ce
Emigdio Espinosa <millosespinosa@gmail.com>
2022-10-17 12:39:47 -0500

    Empezamos a crear el patron de fachada

---
+++ Fachada07/Fachada07/CSistemaCompras.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Subsistemas
+{
+    //Esta clase representa uno de los subsistemas
+    internal class CSistemaCompras
+    {
+        //Este metodo representa alguna operación que puede llevar a cabo el
+        //subsistema, peude haber muchas mas y de mayor complejidad
+
+        public bool Comprar()
+        {
+            string dato = "";
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Introducir numero de tarjeta");
+            dato = Console.ReadLine();
+
+            if (dato == "12345")
+            {
+                Console.ForegroundColor= ConsoleColor.Green;
+                Console.WriteLine("Pago aceptado");
+                return true;
+            }
+            else
+            {
+                Console.ForegroundColor= ConsoleColor.Red;
+                Console.WriteLine("Pago rechazado");
+                return false;   
+            }
+
+        }
+    }
+
+    ////
+    
+    class CSubsistemaAlmacen
+    {
+        private int cantidad; 
+
+        public CSubsistemaAlmacen()
+        {
+            cantidad = 3;
+        }
+
+        public bool SacarAlmacen()
+        {
+            if(cantidad > 0)
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("Producto listo para enviarse");
+                cantidad--;
+                return true;
+            }
+            else
+            {
+                Console.ForegroundColor=ConsoleColor.Red;
+                Console.WriteLine("Producto no disponible, espera a que haya existencias");
+                return false;
+            }
+        }
+
+
+    }
+}

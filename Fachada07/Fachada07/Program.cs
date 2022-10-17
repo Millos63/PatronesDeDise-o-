67cab9a17343af4e4ea08cdf7686bf35bbc0e353
Emigdio Espinosa <millosespinosa@gmail.com>
2022-10-17 12:57:58 -0500

    Terminamos nuestro patron de diseño Fachada07, terminamos el program igual

--- Fachada07/Fachada07/Program.cs
+++ Fachada07/Fachada07/Program.cs
@@ -4,12 +4,42 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
+//Necesario para usar los subsistemas
+using Subsistemas;
+
+
 namespace Fachada07
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            //Creamos la instancia de la fachada
+            CFachada fachada = new CFachada();
+
+            //Hacemos uso de los subsistemas a traves de la interfaz
+            //sencilla de la fachada
+            fachada.Compra();
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("-------");
+
+            fachada.Compra();
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("-------");
+
+            fachada.Compra();
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("-------");
+
+            fachada.Compra();
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("-------");
+
         }
     }
 }

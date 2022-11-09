d08a145d8b74d2dd016944a3dbcd11e7c9622439
Emigdio Espinosa <millosespinosa@gmail.com>
2022-11-09 12:32:18 -0600

    Empezamos a trabajar en el proyecto del 3er parcial

---
+++ 3erParcialPatrones/3erParcialPatrones/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _3erParcialPatrones
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

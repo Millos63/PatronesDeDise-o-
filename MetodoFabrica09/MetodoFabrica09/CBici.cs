92c1b5abb9501452d3f1cea8a827cf3c914b267f
Emigdio Espinosa <millosespinosa@gmail.com>
2022-11-09 12:23:03 -0600

    Termianmos el MetodoFabrica09

--- MetodoFabrica09/MetodoFabrica09/CBici.cs
+++ MetodoFabrica09/MetodoFabrica09/CBici.cs
@@ -6,26 +6,7 @@ using System.Threading.Tasks;
 
 namespace MetodoFabrica09
 {
-    internal class CBici: IVehiculo
+    internal interface CBici
     {
-        public void Encender()
-        {
-            Console.WriteLine("Las bicis no necesitan encendido");
-        }
-
-        public void Acelerar()
-        {
-            Console.WriteLine("Pedalea fuerte para acelerar");
-        }
-
-        public void Frenar()
-        {
-            Console.WriteLine("Presione el freno trasero primero");
-        }
-
-        public void Girar()
-        {
-            Console.WriteLine("Usa el manubrio");
-        }
     }
 }
